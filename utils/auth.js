a78a273014b4092ca6d4cf400fde659a3d1e0832
kdingman <kcdonlan@yahoo.com>
2022-03-09 17:59:36 -0500

    get all posts, single post, create a post, update post, delete post

---
+++ utils/auth.js
@@ -0,0 +1,12 @@
+// authguard (middleware)
+
+const withAuth = (req, res, next) => {
+    if(!req.session.user_id) {
+        res.redirect('/login');
+    }
+    else {
+        next();
+    }
+};
+
+module.exports = withAuth;
