04aaad879d201d972da3528d3a9a69a0d5ef6603
kdingman <kcdonlan@yahoo.com>
2022-03-19 17:11:51 -0400

    final

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -2,16 +2,14 @@ async function editFormHandler(event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="post-title"]').value.trim();
-    const post_content = document.querySelector('textarea[name="post-content]').value.trim();
-    const post_id = window.location.toString().split('/')[
+    const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
 
-    const response = await fetch(`/api/posts/${post_id}`, {
+    const response = await fetch(`/api/post/${id}`, {
         method: 'PUT',
         body: JSON.stringify({
-            title,
-            post_content
+            title
         }),
         headers: { 'Content-Type': 'apolication/json' }
     });
